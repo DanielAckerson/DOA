b32249220c0a149468628718760f5f366a6b225f
Daniel <dj.ackerson1@gmail.com>
2014-11-15 08:36:01 -0500

    added basic game methods

--- DOA/Game.cpp
+++ DOA/Game.cpp
@@ -1,11 +1,81 @@
 #include "Game.h"
-
+#include <iostream>
+#include <exception>
 
 Game::Game()
+    :
+    running(true)
 {
+    if(SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        throw std::exception("Could not initialize SDL");
+    }
+    window = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    if(window == nullptr)
+    {
+        std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        throw std::exception("Could not create SDL window");
+    }
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+    if(renderer == nullptr)
+    {
+        SDL_DestroyWindow(window);
+        std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        throw std::exception("Could not create SDL renderer");
+    }
+    screen.setRenderer(renderer);
 }
 
 
 Game::~Game()
 {
+    saveGameState();
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+
+void Game::tick()
+{
+
+}
+
+
+void Game::draw()
+{
+
+}
+
+
+
+void Game::handleEvent(SDL_Event& event)
+{
+    switch(event.type)
+    {
+    case SDL_QUIT:
+        stop();
+    }
+}
+
+
+void Game::saveGameState()
+{
+
+}
+
+
+void Game::stop()
+{
+    running = false;
+}
+
+
+bool Game::isRunning()
+{
+    return running;
 }
--- DOA/Game.h
+++ DOA/Game.h
@@ -1,8 +1,37 @@
 #pragma once
+
+#include <SDL.h>
+#include <memory>
+
+#include "Screen.h"
+#include "GameState.h"
+
 class Game
 {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
+    Screen screen;
+    std::unique_ptr<GameState> gameState; //needs initialization
+
+    bool running;
+
+    //int fps;
+    //int tps;
+
 public:
     Game();
     ~Game();
+
+    void tick();
+    void draw();
+    void handleEvent(SDL_Event& event);
+
+    //void setTickPerSecond(int tps);
+    //void getTicksPerSecond();
+    //void getFramesPerSecond();
+    void saveGameState();
+    void stop();
+    bool isRunning();
 };
 
